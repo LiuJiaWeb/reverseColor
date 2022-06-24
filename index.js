2d3f38ff98bd1539252c303c99d99500260c05ba
liujia <460705950@qq.com>
2022-06-24 16:17:08 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * @description: 得到互补色
+ * @params originalColor: 原始色(16进制色)
+ * @return String 互补色
+ */
+const func = (originalColor) => {
+  let color = originalColor
+
+  //去除#
+  if (color.includes('#')) {
+    color = color.substring(1)
+  }
+
+  //长度为3位的缩写值，转为完整6位值
+  if (color.length === 3) {
+    color = color.replace(/(\w)(\w)(\w)/, '$1$1$2$2$3$3')
+  }
+
+  const colorList = color.match(/\w{2}/g) //转为数组
+
+  let result = []
+
+  for (let i = 0; i < colorList.length; i++){
+    let cur = 255 - parseInt(colorList[i], 16)
+    cur = cur.toString(16)
+    result.push(cur)
+  }
+
+  result = `#${result.join('')}`
+
+  return result
+}
+
+export default func
